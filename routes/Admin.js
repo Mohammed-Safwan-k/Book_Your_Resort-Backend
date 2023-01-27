d9fc7406b575a4ba2b5865aea32ee9840127a9ae
Mohammed Safwan <safwanpklr@gmail.com>
2023-01-27 22:02:43 +0530

    coupon routes added

--- routes/Admin.js
+++ routes/Admin.js
@@ -2,11 +2,16 @@ import express from "express";
 import {
   addFacilities,
   allFacilities,
+  allCoupon,
+  addCoupon,
 } from "../controllers/adminController.js";
 
 const router = express.Router();
 
 router.post("/addFacilities", addFacilities);
 router.get("/allFacilities", allFacilities);
+
+router.post("/allCoupon", allCoupon);
+router.get("/addCoupon", addCoupon);
 
 export default router;
