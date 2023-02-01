5129e8f5b338da26af92cf9477a3dd4f452daf18
Mohammed Safwan <safwanpklr@gmail.com>
2023-02-01 19:45:42 +0530

    user model changes done

--- models/User.js
+++ models/User.js
@@ -30,6 +30,10 @@ const UserSchema = new mongoose.Schema(
       default: "",
     },
     location: String,
+    status: {
+      type: String,
+      default: "Unblocked",
+    },
   },
   { timestamps: true }
 );
